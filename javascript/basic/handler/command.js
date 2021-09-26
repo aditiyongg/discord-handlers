9188c9478d499a2f0f36971ddb4528c5257baa86
BulletTide <cameronsmith296@gmail.com>
2021-09-26 13:46:07 +0200

    added jsdoc stuff

--- javascript/basic/handler/command.js
+++ javascript/basic/handler/command.js
@@ -9,6 +9,7 @@
 // @ts-check
 class HandlerCommand {
     /**
+     * Custom class for slash commands.
      * @param {import('../src/utils/client')} client The client isntance.
      * @param {CommandOptions} options Settings for the slash command.
      */
@@ -56,7 +57,7 @@ class HandlerCommand {
 
 module.exports = HandlerCommand;
 
-/** @param {Object.<string, import('./command').SubcommandGroup>} groups */
+/** @param {Object.<string, SubcommandGroup>} groups */
 function getSubcommandGroupOptions (groups) {
     const names = Object.keys(groups);
     const options = [];
@@ -76,7 +77,7 @@ function getSubcommandGroupOptions (groups) {
     return options;
 }
 
-/** @param {Object.<string, import('./command').Subcommand>} subcommands */
+/** @param {Object.<string, Subcommand>} subcommands */
 function getSubcommandOptions (subcommands) {
     const names = Object.keys(subcommands);
     const options = [];
@@ -97,54 +98,54 @@ function getSubcommandOptions (subcommands) {
 }
 
 /**
- * @typedef CommandOptions
+ * @typedef CommandOptions Options for the slash command.
  * @type {object}
- * @property {string} name - The name of the slash command
- * @property {string} description - The description of the slash command
- * @property {string} [category] - The category of this command
- * @property {import('discord.js').ApplicationCommandOptionData[]} [options] - The options for this slash command
- * @property {boolean} [defaultPermission] - If false, this slash command will be disabled for everyone
- * @property {import('discord.js').ApplicationCommandPermissionData[]} [permissions] - Data to give/take the permissions for a user/role to use this command
- * @property {boolean} [development] - Whether this is a global command or a guild only command (first ID of config.TEST_SERVERS)
- * @property {boolean} [devOnly] - Whether this command can only be used by a bot dev
- * @property {boolean} [hideCommand] - Whether or not this command will be displayed in the help command
- * @property {boolean} [ownerOnly] - Whether this command can only be used by the server owner
- * @property {boolean} [guildOnly] - Whether this command can only be used in a server
- * @property {import('discord.js').PermissionString[]} [perms] - Permissions that the user needs in order to use this command
- * @property {import('discord.js').PermissionString[]} [clientPerms] - Permissions that the client needs to run this command
- * @property {boolean} [nsfw] - Whether or not this command can only be used in a NSFW channel
- * @property {Object.<string, SubcommandGroup>} [groups] - Subcommand groups for this command
- * @property {Object.<string, Subcommand>} [subcommands] - Subcommands for this command
+ * @property {string} name The name of the slash command.
+ * @property {string} description The description of the slash command.
+ * @property {string} [category] The category of this command.
+ * @property {import('discord.js').ApplicationCommandOptionData[]} [options] The options for this slash command.
+ * @property {boolean} [defaultPermission] If false, this slash command will be disabled for everyone.
+ * @property {import('discord.js').ApplicationCommandPermissionData[]} [permissions] Data to give/take the permissions for a user/role to use this command.
+ * @property {boolean} [development] Whether this is a global command or a guild only command (first ID of config.DEV_SERVERS).
+ * @property {boolean} [devOnly] Whether this command can only be used by a bot dev.
+ * @property {boolean} [hideCommand] Whether or not this command will be displayed in the help command.
+ * @property {boolean} [ownerOnly] Whether this command can only be used by the server owner.
+ * @property {boolean} [guildOnly] Whether this command can only be used in a server.
+ * @property {import('discord.js').PermissionString[]} [perms] Permissions that the user needs in order to use this command.
+ * @property {import('discord.js').PermissionString[]} [clientPerms] Permissions that the client needs to run this command.
+ * @property {boolean} [nsfw] Whether or not this command can only be used in a NSFW channel.
+ * @property {Object.<string, SubcommandGroup>} [groups] Subcommand groups for this command.
+ * @property {Object.<string, Subcommand>} [subcommands] Subcommands for this command.
  */
 
 /**
- * @typedef SubcommandGroup 
+ * @typedef SubcommandGroup The group for a subcommand.
  * @type {object} 
- * @property {string} description 
- * @property {Object.<string, Subcommand>} subcommands 
+ * @property {string} description The description for the subcommand group.
+ * @property {Object.<string, Subcommand>} subcommands An object containing each subcommand.
  */
 
 /**
- * @typedef Subcommand 
+ * @typedef Subcommand The subcommand.
  * @type {object} 
- * @property {string} description 
- * @property {Argument[]} [args] 
- * @property {({ client, interaction, group, subcommand } : { client: import('../src/utils/client'), interaction: import('discord.js').CommandInteraction, group: string, subcommand: string }) => any} [execute] 
+ * @property {string} description The description for the subcommand.
+ * @property {Argument[]} [args] Argument(s) (options) for the subcommand.
+ * @property {({ client, interaction, group, subcommand } : { client: import('../src/utils/client'), interaction: import('discord.js').CommandInteraction, group: string, subcommand: string }) => any} [execute] The execute function for the subcommand.
  */
 
 /**
- * @typedef Argument 
+ * @typedef Argument The argument (options) for the slash command.
  * @type {object} 
- * @property {('STRING'|'INTEGER'|'BOOLEAN'|'USER'|'CHANNEL'|'ROLE'|'MENTIONABLE'|'NUMBER')} type 
- * @property {string} name 
- * @property {string} description 
- * @property {Choice[]} [choices] 
- * @property {boolean} [required] 
+ * @property {('STRING'|'INTEGER'|'BOOLEAN'|'USER'|'CHANNEL'|'ROLE'|'MENTIONABLE'|'NUMBER')} type The type of option for the slash command.
+ * @property {string} name The name for the argument (this needs to be unique for each argument, no spaces!).
+ * @property {string} description The description for the argument.
+ * @property {Choice[]} [choices] The choices for the argument.
+ * @property {boolean} [required] Whether the argument is required or not.
  */
 
 /**
- * @typedef Choice 
+ * @typedef Choice The custom choice for the argument.
  * @type {object} 
- * @property {string} name 
- * @property {string|number} value 
+ * @property {string} name The name of the choice (this needs to be unique for each chouce, no spaces!).
+ * @property {string|number} value The custom value that the choice will return upon interaction (this needs to be unique for each chouce, no spaces!).
  */
--- typescript/basic/handler/command.ts
+++ typescript/basic/handler/command.ts
@@ -6,7 +6,7 @@
           the main repo.
 */
 
-import { PermissionString, Collection, ApplicationCommandPermissionData, CommandInteraction, ApplicationCommandOptionData, ApplicationCommandData } from 'discord.js';
+import { PermissionString, ApplicationCommandPermissionData, CommandInteraction, ApplicationCommandOptionData } from 'discord.js';
 import { Client } from '../src/utils/client';
 
 class HandlerCommand {
@@ -29,9 +29,9 @@ class HandlerCommand {
     subcommands: { [x: string]: Subcommand } | null;
 
     constructor (client: Client, {
-        name = "",
-        description = "",
-        category = "No category",
+        name = '',
+        description = '',
+        category = 'No category',
         options = [],
         defaultPermission = true,
         permissions = [],
@@ -99,7 +99,7 @@ function getSubcommandOptions (subcommands: { [x: string]: Subcommand }) {
             name,
             description: subcommands[name].description,
             options: subcommands[name].args,
-            type: "SUB_COMMAND"
+            type: 'SUB_COMMAND'
         }
 
         options.push(option);
@@ -108,31 +108,27 @@ function getSubcommandOptions (subcommands: { [x: string]: Subcommand }) {
     return options;
 }
 
-declare interface SubcommandGroup {
-    description: string;
-    subcommands: { [x: string]: Subcommand }
-}
-
-declare interface Subcommand {
-    description: string;
-    args?: Argument[];
-    execute? ({ client, interaction, group, subcommand }: { client: Client, interaction: CommandInteraction, group: string, subcommand: string }): any;
-}
-
-declare interface Argument {
-    type: 'STRING'|'INTEGER'|'BOOLEAN'|'USER'|'CHANNEL'|'ROLE'|'MENTIONABLE'|'NUMBER';
-    name: string;
-    description: string;
-    choices?: Choice[];
-    required?: boolean;
-}
-
-declare interface Choice {
-    name: string;
-    value: string|number;
-}
-
-declare interface CommandOptions {
+/**
+ * @typedef CommandOptions Options for the slash command.
+ * @type {object}
+ * @property {string} name The name of the slash command.
+ * @property {string} description The description of the slash command.
+ * @property {string} [category] The category of this command.
+ * @property {import('discord.js').ApplicationCommandOptionData[]} [options] The options for this slash command.
+ * @property {boolean} [defaultPermission] If false, this slash command will be disabled for everyone.
+ * @property {import('discord.js').ApplicationCommandPermissionData[]} [permissions] Data to give/take the permissions for a user/role to use this command.
+ * @property {boolean} [development] Whether this is a global command or a guild only command (first ID of config.DEV_SERVERS).
+ * @property {boolean} [devOnly] Whether this command can only be used by a bot dev.
+ * @property {boolean} [hideCommand] Whether or not this command will be displayed in the help command.
+ * @property {boolean} [ownerOnly] Whether this command can only be used by the server owner.
+ * @property {boolean} [guildOnly] Whether this command can only be used in a server.
+ * @property {import('discord.js').PermissionString[]} [perms] Permissions that the user needs in order to use this command.
+ * @property {import('discord.js').PermissionString[]} [clientPerms] Permissions that the client needs to run this command.
+ * @property {boolean} [nsfw] Whether or not this command can only be used in a NSFW channel.
+ * @property {Object.<string, SubcommandGroup>} [groups] Subcommand groups for this command.
+ * @property {Object.<string, Subcommand>} [subcommands] Subcommands for this command.
+ */
+ declare interface CommandOptions {
     name: string;
     description: string;
     category?: string;
@@ -149,4 +145,56 @@ declare interface CommandOptions {
     nsfw?: boolean;
     groups?: { [x: string]: SubcommandGroup } | null;
     subcommands?: { [x: string]: Subcommand } | null;
+}
+
+/**
+ * @typedef SubcommandGroup The group for a subcommand.
+ * @type {object} 
+ * @property {string} description The description for the subcommand group.
+ * @property {Object.<string, Subcommand>} subcommands An object containing each subcommand.
+ */
+declare interface SubcommandGroup {
+    description: string;
+    subcommands: { [x: string]: Subcommand }
+}
+
+/**
+ * @typedef Subcommand The subcommand.
+ * @type {object} 
+ * @property {string} description The description for the subcommand.
+ * @property {Argument[]} [args] Argument(s) (options) for the subcommand.
+ * @property {({ client, interaction, group, subcommand } : { client: import('../src/utils/client'), interaction: import('discord.js').CommandInteraction, group: string, subcommand: string }) => any} [execute] The execute function for the subcommand.
+ */
+declare interface Subcommand {
+    description: string;
+    args?: Argument[];
+    execute? ({ client, interaction, group, subcommand }: { client: Client, interaction: CommandInteraction, group: string, subcommand: string }): any;
+}
+
+/**
+ * @typedef Argument The argument (options) for the slash command.
+ * @type {object} 
+ * @property {('STRING'|'INTEGER'|'BOOLEAN'|'USER'|'CHANNEL'|'ROLE'|'MENTIONABLE'|'NUMBER')} type The type of option for the slash command.
+ * @property {string} name The name for the argument (this needs to be unique for each argument, no spaces!).
+ * @property {string} description The description for the argument.
+ * @property {Choice[]} [choices] The choices for the argument.
+ * @property {boolean} [required] Whether the argument is required or not.
+ */
+declare interface Argument {
+    type: 'STRING'|'INTEGER'|'BOOLEAN'|'USER'|'CHANNEL'|'ROLE'|'MENTIONABLE'|'NUMBER';
+    name: string;
+    description: string;
+    choices?: Choice[];
+    required?: boolean;
+}
+
+/**
+ * @typedef Choice The custom choice for the argument.
+ * @type {object} 
+ * @property {string} name The name of the choice (this needs to be unique for each chouce, no spaces!).
+ * @property {string|number} value The custom value that the choice will return upon interaction (this needs to be unique for each chouce, no spaces!).
+ */
+declare interface Choice {
+    name: string;
+    value: string|number;
 }
